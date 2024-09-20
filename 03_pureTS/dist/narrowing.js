20900b1b9e5294d4c186359b3ebcbd863959e586
Deepthi Purijala <saideepthi.p@veltris.com>
2024-09-20 18:16:52 +0530

    narrowing in TS

--- 03_pureTS/dist/narrowing.js
+++ 03_pureTS/dist/narrowing.js
@@ -15,3 +15,24 @@ function getFood(pet) {
     }
     return 'bird food';
 }
+function getArea(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * Math.pow(shape.radius, 2);
+    }
+    // return shape.side * shape.side
+}
+// never type
+// we added default case as never so that even if in the future if any other interface gets added, 
+// we will be getting an error to add that interface in the switch case as well so that the code is scalable
+// we made default as never because we are not supposed to run it and we should have case for each case
+function getArea2(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * Math.pow(shape.radius, 2);
+        case "square":
+            return shape.side * shape.side;
+        default:
+            const defaultForArea = shape;
+            return defaultForArea;
+    }
+}
